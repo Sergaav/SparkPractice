7f43f581dc2cc8c8b8a4b7f3cdec5a40ea715c66
Sergaav <sergaav@gmail.com>
2021-07-30 00:16:49 +0300

    Main class commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import javax.xml.crypto.Data;
+
 import static org.apache.spark.sql.functions.*;
 
 public class Main {
@@ -17,10 +19,20 @@ public class Main {
         Dataset<Row> teamsTable = session.read().option("infernSchema", "true")
                 .option("header", "true").csv("/home/serha/sparkHomework/Teams.csv");
 
-        Dataset<Row> awardsCount = awardsTable.groupBy("playerID").count();
-        Dataset<Row> goalsCount = scoringTable.groupBy("playerID").agg(sum("G").as("goals"));
-        awardsCount.join(goalsCount, "playerID").sort(desc("goals"));
-        masterTable.join(scoringTable, "playerID")
-                .join(teamsTable, "tmID").groupBy("playerID");
+        Dataset<Row> awardsCount = awardsTable.groupBy("playerID").count().as("Awards");
+        Dataset<Row> goalsCount = scoringTable.groupBy("playerID").agg(sum("G").as("Goals"));
+        Dataset<Row> countGoals = awardsCount.join(goalsCount, "playerID").sort(desc("Goals"));
+
+        Dataset<Row> playerGoals = countGoals.join(masterTable, "playerId")
+                .select(col("playerID"), concat(col("firstName"), lit(" "), col("lastName")).as("FullName")
+                        , col("Awards.count"), col("Goals"));
+
+        Dataset<Row> scoringTeams = scoringTable.join(teamsTable, "tmID")
+                .groupBy("playerID").agg(collect_set("name").as("Teams"));
+
+        Dataset<Row> finalSet = playerGoals.join(scoringTeams, "playerID").orderBy(desc("goals"));
+        finalSet.repartition(5).write().format("parquet").save("/home/serha/sparkHomework/out");
+
+
     }
 }
